fe6cc7554b1d756bcce461e31adae5740f08abcb
BenKelly99 <benjaminkelly@me.com>
2017-04-18 08:51:51 -0400

    should work with buttons

--- LightRelay/LightRelay.ino
+++ LightRelay/LightRelay.ino
@@ -4,7 +4,6 @@
 #include <Ethernet.h>
 #include <SPI.h>
 boolean reading = false;
-String lastCommand = "";
 byte ip[] = {192, 168, 0, 2};  
 byte gateway[] = {192, 168, 0, 1}; 
 byte subnet[] = {255, 255, 0, 0}; 
@@ -18,13 +17,9 @@ EthernetServer server = EthernetServer(80); //80 = http communication
 
 void setup(){
   pinMode(7, OUTPUT);  //Set the relay control pin to output mode
-
   Ethernet.begin(mac, ip, gateway, subnet);
   pinMode(LED_BUILTIN, OUTPUT);
   server.begin();
-  
-  Serial.println(Ethernet.localIP());
-
 }
 
 void loop(){
@@ -41,7 +36,7 @@ void checkForClient(){
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     boolean sentHeader = false;
-    lastCommand="";
+    String lastCommand="";
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
@@ -68,11 +63,21 @@ void checkForClient(){
                     client.println("<title>Arduino Web Page</title>");
                     client.println("</head>");
                     client.println("<body>");
-                    client.println("<h1>Hello from Arduino!</h1>");
-                    client.println("<p>A web page from the Arduino server</p>");
-                    client.println("<input type=\"button\" value=\"On\" onclick=\"window.location.href='?o'\"/>");
-                    client.println("<input type=\"button\" value=\"Off\" onclick=\"window.location.href='?f'\"/>");
-                    client.println("<input type=\"button\" value=\"Toggle\" onclick=\"window.location.href='http://192.168.0.2/?t'\"/>");
+                    //if(currentState){
+                        //client.println("<p>");
+                        //client.println("<h1>The Heat Rises...</h1>");
+                        //client.println("<iframe src=\"//giphy.com/embed/NrqUE766fNTXO\" width=\"480\" height=\"360\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/NrqUE766fNTXO\">via GIPHY</a></p>");
+                        //client.println("<input type=\"button\" style = \"postion: absolute; top: 450; left: (450-75)/2; height: 20px; width: 50px; background-color:red; cursor:pointer\" value=\"On\" onclick=\"window.location.href='?o'\"/>");
+                        //client.println("<input type=\"button\" style = \"postion: absolute; top: 450; left: (450-75)/2; height: 20px; width: 50px; background-color:white; cursor:pointer\" value=\"Off\" onclick=\"window.location.href='?f'\"/>");
+                   // }
+                   //else{
+                        client.println("</p>");
+                        client.println("<h1>It's Tea Time</h1>");
+                        client.println("<img src=\"https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/4e14cd59-bc1c-4f0b-8ad4-26cf216c2d5d.jpg._CB290777952_.jpg\" alt = \"image not found\" style=\"width:450px;height:450px;\"><br />");
+                        client.println("<input type=\"button\" style = \"postion: absolute; top: 450; left: (450-75)/2; height: 20px; width: 50px; background-color:white; cursor:pointer\" value=\"On\" onclick=\"window.location.href='?o'\"/>");
+                        client.println("<input type=\"button\" style = \"postion: absolute; top: 450; left: (450-75)/2; height: 20px; width: 50px; background-color:red; cursor:pointer\" value=\"Off\" onclick=\"window.location.href='?f'\"/>");
+                    //}
+                    client.println("<input type=\"button\" style = \"height: 20px; width: 50px; background-color:white; cursor:pointer\" value=\"Toggle\" onclick=\"window.location.href='http://192.168.0.2/?t'\"/>");
                     client.println("</body>");
                     client.println("</html>");
                     break;  //If 2 /ns are recieved, end the reading phase
